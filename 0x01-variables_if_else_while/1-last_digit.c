a13f1776640640d887cb3df8c9e14229cf41605d
wrochi99 <raymondwameyo@gmail.com>
2022-09-09 20:11:25 -0700

    Hopefully solved the error with this program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,23 +4,35 @@
 #include <stdio.h>
 /*
  * main - printing
- * description - printing last digit 
+ * description - printing last digit
  * description - confirming whether it's greater than 5
  * description - confirming if it's zero
  * description - confirming if it's less than 6 and not 0
  *
  * return 0 (successful)
- */ 
+ */
 int main(void)
 {
 		int n;
+		char lastDigit[]= "Last digit of";
+
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 
 		/* my code */
 		/* declare var_name */
-		char lastDigit[] = "Last digit of";
-		printf("%s %d is %d and is ", last, n, n % 10);
-		if n
+		printf("%s %d is %d and is ", lastDigit, n, n % 10);
+		if (n > 5)
+		{
+			printf("greater than 5\n");
+		}
+		else  if (n == 0)
+		{
+			printf("is 0\n");
+		}
+		else
+		{
+			printf("less than 6 and not 0\n");
+		}
 		return (0);
 }
