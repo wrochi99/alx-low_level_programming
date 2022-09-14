14c63f898f39ab3e80e2aba710e84fa5eda0315d
wrochi99 <raymondwameyo@gmail.com>
2022-09-14 14:09:42 -0700

    ...

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,20 +8,17 @@
  * Return: 0
  */
 int main(void)
-{	
+{
 	char c;
 
-	for (c = '_'; c <= 'z';)
-	{
-		_putchar("_");
-		_putchar("p");
-		_putchar("u");
-		_putchar("t");
-		_putchar("c");
-		_putchar("h");
-		_putchar("a");
-		_putchar("r");
-	}
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	_putchar("\n");
 	return (0);
 }
