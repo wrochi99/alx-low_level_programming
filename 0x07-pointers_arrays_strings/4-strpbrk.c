0ed56868ebf27c00988854f81ab98bb5bcf7460d
wrochi99 <raymondwameyo@gmail.com>
2022-09-27 12:27:23 -0700

    Task 4 correction attempt

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,16 +10,16 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i > 0; *(s + i); i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; *(accept + j); j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				break;
 			}
 		}
-		if (!accept[j])
+		if (accept[j])
 		{
 			return (s + i);
 		}
