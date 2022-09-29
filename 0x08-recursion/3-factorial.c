dfda52d8bfe8feed205e6c5aaacfc331b7573b14
wrochi99 <raymondwameyo@gmail.com>
2022-09-29 05:57:44 -0700

    Task 3 correction attempt

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -19,7 +19,7 @@ int factorial(int n)
 	}
 	if (n >= 1)
 	{
-		return (n * factorial(n - 1));
+		return ((n * factorial(n - 1) * -1));
 	}
-	return (n * factorial(n - 1));
+	return ((n * (factorial(n - 1) * -1));
 }
