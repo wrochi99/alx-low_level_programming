571fe404327ae9098d40f5369c9156ed50b54949
wrochi99 <raymondwameyo@gmail.com>
2022-09-29 04:19:24 -0700

    Task 0 correction attempt

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,22 +2,21 @@
 
 /**
  * _puts_recursion - prints string followed by new line
- *
- * @s - string to be printed
+ * @s: string to be printed
  *
  * Return: void
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	int i;
-	char s[i];
+	char *s[i];
 
 	if (i == 0)
 	{
 		_putchar('\n');
 	}
 
-	while (i > 0)
+	if (i > 0)
 	{
 		_putchar(s[i]);
 		i++;
