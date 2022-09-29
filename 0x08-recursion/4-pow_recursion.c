e8284c9dadd2060fb500fd1e647dcbfb47ff17e9
wrochi99 <raymondwameyo@gmail.com>
2022-09-29 06:13:07 -0700

    Task 4 correction attempt

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,11 +15,11 @@ int _pow_recursion(int x, int y)
 	}
 	else if (y == 0)
 	{
-		return (x);
+		return (1);
 	}
 	else if (y > 0)
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
-	return (0);
+	return (x * _pow_recursion(x, y - 1);
 }
