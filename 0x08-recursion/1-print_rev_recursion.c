c8220d40e06ef761ee7f8b3a712f63fbe919a7a9
wrochi99 <raymondwameyo@gmail.com>
2022-09-29 05:13:12 -0700

    Task 1 correction attempt

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -14,8 +14,8 @@ void _print_rev_recursion(char *s)
 	_print_rev_recursion(s + 1);
 	if (*s == '\0')
 	{
-		s[i] == '\0';
+		s[i] = (*s == '\0');
 		_putchar(s[i]);
-		_print_rev_recursions(s[i] - 1)
+		_print_rev_recursion(s[i] - 1);
 	}
 }
