d03dc7f3ab2a505d814cc80d12217ceeb765378f
wrochi99 <raymondwameyo@gmail.com>
2022-09-29 05:31:39 -0700

    Task 2 correction attempt

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _strlen_recursion - return length of a string
  * @s: source string
- * 
+ *
  * Return: 0
  */
 int _strlen_recursion(char *s)
@@ -13,10 +13,11 @@ int _strlen_recursion(char *s)
 	int i;
 
 	i = 0;
-	
+
 	if (s[i] != '\0')
 		{
 			i++;
+			i++ += _strlen_recursion(s + 1);
 		}
 	return (i);
 }
