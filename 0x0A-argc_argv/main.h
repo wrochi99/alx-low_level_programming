e414964b980a6da0d45ef10a7d89e6b57c975efa
wrochi99 <raymondwameyo@gmail.com>
2022-09-30 16:19:32 -0700

    Updating prototypes in the main header file

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c);
+int what_is_my_name(int argc, char *argv[]);
+int print_num_arg(int argc, char* argv[]);
+int print_all_arg(int argc, char *argv[]);
+int multiply_two_num(int argc, char *argv[]);
+int only_positive_sum(int argc, char *argv[]);
 
 #endif
