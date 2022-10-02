68044df297af65d3521d26d26e535550ff8961e0
wrochi99 <raymondwameyo@gmail.com>
2022-10-02 13:02:40 -0700

    Task 4 correction update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,40 +3,32 @@
 #include <stdlib.h>
 
 /**
- * only_positive_sum - prints result of (x + y + ....)
+ * main - prints result of (x + y + ....)
  * followed by a new line
  * @argc: argument counter
  * @argv: argument vector
- * 
- * Return: 0 if successful
- * 	   1 if error
+ *
+ * Return: 0 if successful or 1 else
  */
-int only_positive_sum(int argc)
+int main(int argc, char *argv[])
 {
 	/**
 	 * index for argv[]
-	 */	
+	 */
 	int i, result;
+	(void) argv;
 
-	if (argc > 0)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		if (i >= 1)
 		{
-			if (i < 0)
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else if (i >= 1)
-			{
-				result = result + i;
-				printf("%d\n", result);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			result = result + i;
+			printf("%d\n", result);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
 		}
 	}
 	return (0);
